b8facc099d4ce0783671ce01b8d6505e79027786
Dragan Opric <opric.dragan@gmail.com>
2021-10-05 06:59:00 +0200

    recipe class added

---
+++ Models/IRecipeService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cakefactory.Models
+{
+	public interface IRecipeService
+	{
+		Task DeleteAsync(long id);
+		Recipe Find(long id);
+		Task<Recipe> FindAsync(long id);
+		IQueryable<Recipe> GetAll(int? count = null, int? page = null);
+		Task<Recipe[]> GetAllAsync(int? count = null, int? page = null);
+		Task SaveAsync(Recipe recipe);
+	}
+}
---
+++ Models/RecipesDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using cakefactory.Extensions;
+
+namespace cakefactory.Models
+{
+	public class RecipesDbContext : DbContext
+	{
+		public DbSet<Recipe> Recipes { get; set; }
+
+		public RecipesDbContext(DbContextOptions<RecipesDbContext> options)
+			: base(options)
+		{
+			this.EnsureSeedData();
+		}
+	}
+}
