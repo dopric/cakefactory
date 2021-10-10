5d308762f3f369067b92c354d0d7930ab96757a0
Dragan Opric <opric.dragan@gmail.com>
2021-10-10 18:28:02 +0200

    updated

--- Startup.cs
+++ Startup.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 
 namespace cakefactory
@@ -24,7 +27,13 @@ namespace cakefactory
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
-        }
+            //services.AddAuthorization(options =>
+            //{
+	           // options.FallbackPolicy = new AuthorizationPolicyBuilder()
+		          //  .RequireAuthenticatedUser()
+		          //  .Build();
+            //});
+      }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -42,8 +51,14 @@ namespace cakefactory
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            //app.UseStaticFiles(new StaticFileOptions
+            //{
+	           // FileProvider = new PhysicalFileProvider(
+		          //  Path.Combine(Directory.GetCurrentDirectory(), "img")),
+	           // RequestPath = "/img"
+            //});
 
-            app.UseRouting();
+         app.UseRouting();
 
             app.UseAuthorization();
 
